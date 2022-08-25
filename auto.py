f81145143ce9532565a1f519842f806942f65833
UtkarshHiranwal <111518244+UtkarshHiranwal@users.noreply.github.com>
2022-08-25 09:20:52 +0530

    Add files via upload

---
+++ auto.py
@@ -0,0 +1,43 @@
+import pyautogui
+from PIL import Image,ImageGrab
+import time
+def hit(key):
+    pyautogui.keyDown(key)
+    return
+def iscol(data):
+    for i in range(220, 250):
+        for j in range(420, 450):
+            if data[i, j] in range(70,200):
+                hit('up')
+                return
+    for i in range(170, 230):
+        for j in range(300, 380):
+            if data[i, j] in range(70,200):
+                hit('down')
+                return
+    return
+
+
+
+def takeScreenshot():
+    image= ImageGrab.grab()
+    image.show()
+    return image
+if __name__=="__main__":
+    time.sleep(3)
+    while True:
+        image=ImageGrab.grab().convert('L')
+        data=image.load()
+        iscol(data)
+    #     for i in range(190,230):
+    #         for j in range (420,490):
+    #             data[i,j]=0
+    #     for i in range(170,230):
+    #         for j in range (300,380):
+    #             data[i,j]=170
+    #     break
+    # image.show()
+
+
+
+
