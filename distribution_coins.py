93bd7c3583c62a2c697e323624f77df2bd7b3e95
UtkarshHiranwal <111518244+UtkarshHiranwal@users.noreply.github.com>
2022-08-25 09:45:24 +0530

    Add files via upload

---
+++ distribution_coins.py
@@ -0,0 +1,18 @@
+def dist(s):
+    one = 0
+    two = 0
+    five = int((s-4)/5)
+    print(five)
+    if ((s-5*five)%2==0):
+        one=2
+    else:
+        one=1
+    two=int((s-5*five-one)/2)
+    print("Total numbers of coins: ",one+two+five)
+    print("Total numbers of five coins: ",five)
+    print("Total numbers of two coins: ",two)
+    print("Total numbers of one coins: ",one)
+
+if __name__=="__main__":
+    s = int(input("enter number : "))
+    dist(s)
