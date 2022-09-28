5831a4f582b62fc54e60c6da74db0584372280e6
UtkarshHiranwal <111518244+UtkarshHiranwal@users.noreply.github.com>
2022-09-28 21:15:08 +0530

    Makes a word cloud from uploaded .txt file
    It was the project from google in which I had to complete the code and run it as it shows the cloud.

--- Word_Cloud.py
+++ Word_Cloud.py
@@ -1,22 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# # Final Project - Word Cloud
-
-# For this project, you'll create a "word cloud" from a text by writing a script.  This script needs to process the text, remove punctuation, ignore case and words that do not contain all alphabets, count the frequencies, and ignore uninteresting or irrelevant words.  A dictionary is the output of the `calculate_frequencies` function.  The `wordcloud` module will then generate the image from your dictionary.
-
-# For the input text of your script, you will need to provide a file that contains text only.  For the text itself, you can copy and paste the contents of a website you like.  Or you can use a site like [Project Gutenberg](https://www.gutenberg.org/) to find books that are available online.  You could see what word clouds you can get from famous books, like a Shakespeare play or a novel by Jane Austen. Save this as a .txt file somewhere on your computer.
-# <br><br>
-# Now you will need to upload your input file here so that your script will be able to process it.  To do the upload, you will need an uploader widget.  Run the following cell to perform all the installs and imports for your word cloud script and uploader widget.  It may take a minute for all of this to run and there will be a lot of output messages. But, be patient. Once you get the following final line of output, the code is done executing. Then you can continue on with the rest of the instructions for this notebook.
-# <br><br>
-# **Enabling notebook extension fileupload/extension...**
-# <br>
-# **- Validating: <font color =green>OK</font>**
-
-# In[7]:
-
-
-# Here are all the installs and imports you will need for your word cloud script and ).system('pip install ipywidgets')
 
 import wordcloud
 import numpy as np
@@ -25,16 +7,6 @@ from IPython.display import display
 import fileupload
 import io
 import sys
-
-
-# Whew! That was a lot. All of the installs and imports for your word cloud script and uploader widget have been completed. 
-# <br><br>
-# **IMPORTANT!** If this was your first time running the above cell containing the installs and imports, you will need save this notebook now. Then under the File menu above,  select Close and Halt. When the notebook has completely shut down, reopen it. This is the only way the necessary changes will take affect.
-# <br><br>
-# To upload your text file, run the following cell that contains all the code for a custom uploader widget. Once you run this cell, a "Browse" button should appear below it. Click this button and navigate the window to locate your saved text file.
-
-# In[8]:
-
 
 # This is the uploader widget
 
